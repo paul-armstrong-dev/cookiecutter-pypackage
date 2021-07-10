2fa06b0809a11249c64748f72561c6e4ed953d90
Paul Armstrong <parmstrong@gitlab.com>
2021-07-10 21:45:24 +0200

    Changed to use requirements.txt files rather, cleaned up more defaults.

--- setup.py
+++ setup.py
@@ -1,30 +1,69 @@
-# !/usr/bin/env python
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-from distutils.core import setup
+import pip
+
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
+
+## workaround derived from: https://github.com/pypa/pip/issues/7645#issuecomment-578210649
+parsed_requirements = parse_requirements(
+    'requirements/prod.txt',
+    session='workaround'
+)
+
+parsed_test_requirements = parse_requirements(
+    'requirements/test.txt',
+    session='workaround'
+)
+
+
+requirements = [str(ir.req) for ir in parsed_requirements]
+test_requirements = [str(tr.req) for tr in parsed_test_requirements]
+
+
 setup(
-    name='cookiecutter-pypackage',
-    packages=[],
-    version='0.1.0',
-    description='Cookiecutter template for a Python package',
-    author='Audrey Roy Greenfeld',
-    license='BSD',
-    author_email='aroy@alum.mit.edu',
-    url='https://github.com/audreyr/cookiecutter-pypackage',
-    keywords=['cookiecutter', 'template', 'package', ],
-    python_requires='>=3.6',
+    name='{{ cookiecutter.project_slug }}',
+    version='{{ cookiecutter.version }}',
+    description="{{ cookiecutter.project_short_description }}",
+    long_description=readme + '\n\n' + history,
+    author="{{ cookiecutter.full_name }}",
+    author_email='{{ cookiecutter.email }}',
+    url='https://github.com/{{ cookiecutter.github_username }}/{{ cookiecutter.project_slug }}',
+    packages=[
+        '{{ cookiecutter.project_slug }}',
+    ],
+    package_dir={'{{ cookiecutter.project_slug }}':
+                 '{{ cookiecutter.project_slug }}'},
+    include_package_data=True,
+    install_requires=requirements,
+    license="ISCL",
+    zip_safe=False,
+    keywords='{{ cookiecutter.project_slug }}',
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Environment :: Console',
+        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
+        'License :: OSI Approved :: ISC License (ISCL)',
         'Natural Language :: English',
-        'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: Implementation :: CPython',
-        'Programming Language :: Python :: Implementation :: PyPy',
-        'Topic :: Software Development',
     ],
+    test_suite='tests',
+    tests_require=test_requirements
 )
